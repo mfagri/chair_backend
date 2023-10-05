74aedfd6f4e67c940a199ea7c2d03b9bf0f7d763
Marouane Fagri <mfagri@e3r7p9.1337.ma>
2023-10-05 21:10:28 +0100

    backend first push

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const expressSession = require("express-session");
+const app = express();
+require('dotenv').config();
+const clientid = process.env.clientID;
+const secret = process.env.clientSecret;
+/////
+app.use(
+  expressSession({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+////
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: clientid,
+      clientSecret: secret,
+      callbackURL: "http://localhost:3000/auth/google/callback", // Your callback URL
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Use the profile information (e.g., profile.id, profile.displayName) to create or authenticate a user.
+      // You can store user information in your database.
+      console.log();
+      return done(null, profile);
+    }
+  )
+);
+// Serialize user information for session storage
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+////
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Redirect to the desired route after successful authentication.
+    res.redirect("/dashboard");
+  }
+);
+
+app.get("/dashboard", (req, res) => {
+  // Display the user's dashboard.
+  res.send(`Welcome, ${req.user.displayName}!`);
+});
+////
+app.get("/api/products", (req, res) => {
+  // Handle GET request for products
+  res.json({ message: "Get products" });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
